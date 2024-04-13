d8b5775091d1125cf0fc48c9a135e9b49c97dee2
Ankush263 <ankushbanik263@gmail.com>
2024-04-14 00:41:11 +0530

    update

--- src/components/comments/comment-list.tsx
+++ src/components/comments/comment-list.tsx
@@ -1,26 +1,25 @@
-import CommentShow from "@/components/comments/comment-show";
+import CommentShow from '@/components/comments/comment-show';
+import { fetchCommentsBypostId } from '@/db/queries/comments';
 
-interface CommentListProps {}
+interface CommentListProps {
+	postId: string;
+}
 
-// TODO: Get a list of comments from somewhere
-export default function CommentList({}: CommentListProps) {
-  const topLevelComments = comments.filter(
-    (comment) => comment.parentId === null
-  );
-  const renderedComments = topLevelComments.map((comment) => {
-    return (
-      <CommentShow
-        key={comment.id}
-        commentId={comment.id}
-        comments={comments}
-      />
-    );
-  });
+export default async function CommentList({ postId }: CommentListProps) {
+	const comments = await fetchCommentsBypostId(postId);
+	const topLevelComments = comments.filter(
+		(comment) => comment.parentId === null
+	);
+	const renderedComments = topLevelComments.map((comment) => {
+		return (
+			<CommentShow key={comment.id} commentId={comment.id} postId={postId} />
+		);
+	});
 
-  return (
-    <div className="space-y-3">
-      <h1 className="text-lg font-bold">All {comments.length} comments</h1>
-      {renderedComments}
-    </div>
-  );
+	return (
+		<div className="space-y-3">
+			<h1 className="text-lg font-bold">All {comments.length} comments</h1>
+			{renderedComments}
+		</div>
+	);
 }
